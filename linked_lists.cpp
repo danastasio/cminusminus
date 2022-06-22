36afa9731e92acc2f6510c6fd92855bcfaba9cd7
David Anastasio <github@davidanastasio.com>
2022-06-22 10:50:00 -0400

    Initial commit

---
+++ linked_lists.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+
+using namespace std;
+class LinkedList
+{
+    private:
+	struct node
+	{
+	    string data;
+	    struct node *next;
+	};
+
+	node *head = NULL;
+	node *tail = NULL;
+	public:
+	int length = 0;
+    void append(string n)
+    {
+        node *tmp = new node;
+        tmp->data = n;
+        tmp->next = NULL;
+
+        if(head == NULL)
+        {
+            head = tmp;
+            tail = tmp;
+        }
+        else
+        {
+            tail->next = tmp;
+            tail = tail->next;
+        }
+		length++;
+    }
+	void print()
+	{
+		node *pointer = head;
+		while(pointer != NULL) {
+			cout << pointer->data << " Location: " << pointer << endl;
+			pointer = pointer->next;
+		}
+	}
+	string get(int x)
+	{
+		node *pointer = head;
+		for (int i=0; i!=x; i++) {
+			if (i == x) {
+				break;
+			} else {
+				pointer = pointer->next;
+			}
+		}
+		return pointer->data;
+	}
+	bool find(string fd)
+	{
+		node *pointer = head;
+		for (int i=0; i!=length; i++) {
+			if (pointer->data == fd) {
+				return true;
+			} else {
+				pointer = pointer->next;
+			}
+		}
+		return false;
+	}
+	void pop()
+	{
+		node *pointer = head;
+		for (int i=0; i<length-2; i++) {
+			pointer = pointer->next;
+		}
+		delete tail;
+		pointer->next = NULL;
+		tail = pointer;
+	}
+};
+
+int main() {
+	LinkedList list;
+	list.append("Moiraine Damodred");
+	list.append("Siuan Sanche");
+	list.append("Rand Al'Thor");
+	list.append("Nynaeve Al'mera");
+	list.append("Mat Cauthon");
+	list.append("Perrin Aybara");
+
+	cout << "\nPrint items in linked list" << endl;
+	list.print();
+
+	cout << "\nGet an element by 'index': " << list.get(4) << endl;
+	cout << "\nMat Cauthon in list: " << list.find("Mat Cauthon") << endl;
+	cout << "\nEgwene Al'vere in list: " << list.find("Egwene Al'vere") << endl;
+
+	cout << "\nRemove last element (pop)" << endl;
+	list.pop();
+
+
+	cout << "\nPrint list again showing removed element gone" << endl;
+	list.print();
+}
